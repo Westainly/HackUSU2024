a6db2c489a447c271248d8574168808880194994
jakob <jakob22johnson@gmail.com>
2024-03-01 22:35:01 -0700

    Added a main call to actually test the ard reader

--- python/ardCon.py
+++ python/ardCon.py
@@ -1,4 +1,5 @@
 import serial as ser
+import time
 
 port = "/dev/ttyACM0"
 
@@ -7,4 +8,12 @@ ard = ser.Serial(port, 9600, timeout=5)
 # This will read what is in the buffer :D
 def recieveData():
     msg = ard.read(ard.in_waiting)
-    print(msg)
+    return msg
+
+def main():
+    for i in range(5):
+        time.sleep(1)
+        print(recieveData())
+
+
+main()
